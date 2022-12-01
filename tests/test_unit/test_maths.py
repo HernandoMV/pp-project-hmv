b3c622c1a827740670e125a8c1d4adb0df13dd1b
HernandoMV <hernybiotec@gmail.com>
2022-12-01 14:08:26 +0000

    added testing

---
+++ tests/test_unit/test_maths.py
@@ -0,0 +1,6 @@
+from pp_project_hmv import maths
+
+def test_add_numbers():
+    assert maths.add_numbers(1, 3) == 4, 'adding 1 and 3 failed'
+    assert maths.add_numbers(10, 100.1) == 110.1, 'adding floats failed'
+    # assert maths.add_numbers(1, 5) == 110.1, 'adding 1 and 5 failed'
