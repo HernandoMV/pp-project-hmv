99af87dab685497e053e1b77e05ba3f4ecf5c51f
HernandoMV <hernybiotec@gmail.com>
2022-12-01 15:33:04 +0000

    fixed bug for old python version

--- pp_project_hmv/maths.py
+++ pp_project_hmv/maths.py
@@ -1,7 +1,9 @@
 from typing import Union
 
 
-def add_numbers(a: int | float, b: Union[int, float]) -> Union[int, float]:
+def add_numbers(
+    a: Union[int, float], b: Union[int, float]
+) -> Union[int, float]:
     return a + b
 
 
